3dac59d640e6e28fae9b88c6cf46d8e7e7669294
Luke Gareth Ribchester <luke@luke.gr>
2025-01-25 12:29:07 +0100

    build(rosa): generate the `availability` controller
    See: #6 #7
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- apps/rosa/src/app/app.module.ts
+++ apps/rosa/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AvailabilityController } from './controllers/availability.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AvailabilityController],
   providers: [AppService],
 })
 export class AppModule {}
