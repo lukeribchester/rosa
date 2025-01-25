83895424ffed95d377b123511a82d11f27cdb119
Luke Gareth Ribchester <luke@luke.gr>
2025-01-25 12:30:40 +0100

    build(rosa): delete the `app` controller
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- apps/rosa/src/app/app.module.ts
+++ apps/rosa/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AvailabilityController } from './controllers/availability.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, AvailabilityController],
+  controllers: [AvailabilityController],
   providers: [AppService],
 })
 export class AppModule {}
