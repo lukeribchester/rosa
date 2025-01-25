209f2ad1069b3b6b3afc5deecf03ed555141d535
Luke Gareth Ribchester <luke@luke.gr>
2025-01-25 12:54:18 +0100

    feat(rosa): create stubs for the `availability` controller and service
    See: #6 #7
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- apps/rosa/src/app/controllers/availability.controller.ts
+++ apps/rosa/src/app/controllers/availability.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+
+import { AvailabilityService } from '../services/availability.service';
 
 @Controller('availability')
-export class AvailabilityController {}
+export class AvailabilityController {
+  constructor(private availabilityService: AvailabilityService) {}
+
+  @Get('list')
+  public listAvailability(
+    @Query('from') from: Date,
+    @Query('to') to: Date
+  ): string {
+    return this.availabilityService.listAvailability(from, to);
+  }
+
+  @Get('next')
+  public nextAvailability(@Query('date') date: Date): string {
+    return this.availabilityService.nextAvailability(date);
+  }
+}
--- apps/rosa/src/app/services/availability.service.ts
+++ apps/rosa/src/app/services/availability.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AvailabilityService {}
+export class AvailabilityService {
+  public listAvailability(to: Date, from: Date): string {
+    return `${to} - ${from}`;
+  }
+
+  public nextAvailability(date: Date): string {
+    return `${date}`;
+  }
+}
