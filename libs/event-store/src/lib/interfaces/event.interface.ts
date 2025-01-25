ab96b09770e62fbea86e95200023604f0eb6de17
Luke Gareth Ribchester <luke@luke.gr>
2025-01-25 14:01:39 +0100

    refactor(event-store): simplify the `event` interfaces
    See: #4
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- libs/event-store/src/lib/interfaces/event.interface.ts
+++ libs/event-store/src/lib/interfaces/event.interface.ts
@@ -1,25 +1,20 @@
 export interface Event {
   id: string;
-  timestamp: Date;
   type: string;
 }
 
 export interface AvailabilityUpdated extends Event {
   type: 'AVAILABILITY_UPDATED';
   payload: {
-    availability: {
-      from: Date;
-      to: Date;
-    };
+    from: Date;
+    to: Date;
   };
 }
 
 export interface AppointmentCreated extends Event {
   type: 'APPOINTMENT_CREATED';
   payload: {
-    appointment: {
-      from: Date;
-      to: Date;
-    };
+    from: Date;
+    to: Date;
   };
 }
