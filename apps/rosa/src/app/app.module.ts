1883e8a4f75bbc3696eac61b738e729392f42f0f
Luke Gareth Ribchester <luke@luke.gr>
2025-01-25 12:31:21 +0100

    build(rosa): delete the `app` service
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- apps/rosa/src/app/app.module.ts
+++ apps/rosa/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { AvailabilityController } from './controllers/availability.controller';
 
 @Module({
   imports: [],
   controllers: [AvailabilityController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
